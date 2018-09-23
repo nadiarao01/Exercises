ad392fe5642c61e38e153d93a99530aabc9023a1
nadiarao <nadiarao@192.168.86.25>
2018-09-22 22:23:14 -0500

    Commit

---
+++ src/exercises/Exercises.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises;
+
+/**
+ *
+ * @author nadiarao
+ */
+import java.util.Scanner;
+public class Exercises {
+
+    /**
+     * @param args the command line arguments
+     */
+     
+   
+    public static void main(String[] args) {
+     //
+     /*
+      Start NadiaSystem.out.println("Hello World");
+       System.out.println("My name is Nadia Rao");
+       
+      
+        System.out.println(" sum of two number: ");
+        System.out.println ( 74 );
+        System.out.println( 36 );
+        
+        System.out.println(74 + 36);
+         
+        System.out.println(" Division of two numbers: ");
+        System.out.println( 50 );
+        System.out.println( 3 );
+        System.out.println( 50 / 3 );
+       
+        
+        //EXAMPLE ONE
+        int a, b, c;
+       
+               a = 74;
+        b = 36;
+        
+        c = a + b;
+        System.out.println("Sum of two numbers:" +  c);
+        
+        c= a/b;
+        System.out.println("Product of two numbers:" + c);
+        
+       
+   Scanner in = new Scanner(System.in);//
+
+   System.out.println("input first number");
+   int num1 = in.nextInt();
+   
+   System.out.println("input second number");
+   int num2 = in.nextInt();
+   int num3 = num1 * num2;
+   
+   System.out.println(num3);
+     
+   
+   
+   Scanner in = new Scanner(System.in);
+   
+   System.out.println("input first number");
+   int num1 = in.nextInt();
+   
+   System.out.println("input second number");
+   int num2 = in.nextInt();
+   
+   System.out.println(num1 + num2);
+   
+   System.out.println(num1 * num2);
+   
+   System.out.println(num1 / num2);
+   
+   System.out.println(num1 % num2); End Nadia
+*/
+     
+    Scanner in = new Scanner(System.in);
+   
+   System.out.println("input number");
+   int num1 = in.nextInt();
+        for(int i = 1; i <= 10; i++)
+    
+        System.out.println(num1 * i );
+       
+   
+   
+    
+    }
+}
+ 
